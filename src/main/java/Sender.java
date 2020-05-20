8fe65484eaa7ae0e06edaa189b0e0f2e8386f73e
Александр <sosnovaya3@gmai.com>
2020-05-20 15:57:09 +0300

    Application creted

---
+++ src/main/java/Sender.java
@@ -0,0 +1,31 @@
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.ConnectionFactory;
+
+public class Sender {
+    private final static String QUEUE_NAME = "queue";
+
+    public static void main(String[] args) throws Exception {
+        ConnectionFactory factory = new ConnectionFactory();
+        factory.setHost("localhost");
+        try (Connection con = factory.newConnection();
+             Channel channel = con.createChannel();
+        ) {
+            channel.queueDeclare(QUEUE_NAME, false, false, false, null);
+            String message = "Hello World!";
+            channel.basicPublish("", QUEUE_NAME, null, message.getBytes());
+            System.out.println(" [x] Sent '" + message + "'");
+
+//            ConnectionFactory factory = new ConnectionFactory();
+//            factory.setHost("localhost");
+//            try (Connection connection = factory.newConnection()) {
+//                Channel channel = connection.createChannel();
+//                channel.queueDeclare(QUEUE_NAME, false, false, false, null);
+//                String message = "Hello World!";
+//                channel.basicPublish("", QUEUE_NAME, null, message.getBytes());
+//                System.out.println(" [x] Sent '" + message + "'");
+//            }
+        }
+    }
+}
+
